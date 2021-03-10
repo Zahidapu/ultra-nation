87f3b8de5e24c29a56be8ebf01aab317417e4f88
Zahidapu <37400394+Zahidapu@users.noreply.github.com>
2021-03-10 23:15:38 +0600

    done

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart=props.cart;
+
+    let totalPopulation = 0;
+    for (let i = 0; i < cart.length; i++) {
+        const country = cart[i];
+        totalPopulation=totalPopulation+country.population;
+        
+    }
+    return (
+        <div>
+            <h4>this is cart: {cart.length}</h4>
+            <p>Total population: {totalPopulation}</p>
+        </div>
+    );
+};
+
+export default Cart;
